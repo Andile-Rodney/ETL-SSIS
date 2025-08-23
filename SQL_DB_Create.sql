af7e1757b0b1e77b179458c131050b4db60cec90
Rodney <42125381+Andile-Rodney@users.noreply.github.com>
2025-08-23 15:05:11 +0200

    Update SQL_DB_Create.sql
    Adding data warehouse scripts

--- SQL_DB_Create.sql
+++ SQL_DB_Create.sql
@@ -19,3 +19,17 @@ CREATE TABLE Customer_Details (
     Email VARCHAR(100),
     Phone VARCHAR(20)
 );
+
+
+-- Data WareHouse Database Creation
+CREATE DATABASE Financial_Data_Warehouse;
+
+--Table creation
+CREATE TABLE Financial_Analysis (
+    Transaction_Id INT PRIMARY KEY,
+    Customer_Name VARCHAR(50),
+    Supplier_Name VARCHAR(50),
+    Transaction_Date DATE,
+    Amount_USD DECIMAL(10, 2),
+    Supplier_Phone VARCHAR(20)
+);
